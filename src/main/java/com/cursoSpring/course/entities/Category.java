77a28cdc7a2066eb62f4b518f6778896570b2801
JoaoVictorMarcal <jmarcalbarbosa@gmail.com>
2025-05-16 10:19:53 -0300

    Many-to-many association with JoinTable

--- src/main/java/com/cursoSpring/course/entities/Category.java
+++ src/main/java/com/cursoSpring/course/entities/Category.java
@@ -1,11 +1,16 @@
 package com.cursoSpring.course.entities;
 
 import java.io.Serializable;
+import java.util.HashSet;
+import java.util.Set;
+
+import com.fasterxml.jackson.annotation.JsonIgnore;
 
 import jakarta.persistence.Entity;
 import jakarta.persistence.GeneratedValue;
 import jakarta.persistence.GenerationType;
 import jakarta.persistence.Id;
+import jakarta.persistence.ManyToMany;
 import jakarta.persistence.Table;
 
 @Entity
@@ -18,6 +23,10 @@ public class Category implements Serializable {
 	@GeneratedValue(strategy = GenerationType.IDENTITY)
 	private Long id;
 	private String name;
+	
+	@JsonIgnore
+	@ManyToMany(mappedBy = "categories")
+	private Set<Product> products = new HashSet<>();
 
 	public Category() {
 	}
