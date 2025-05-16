c89d21dfb8f1924314867afb5c5b3b8d78287414
JoaoVictorMarcal <jmarcalbarbosa@gmail.com>
2025-05-16 13:11:06 -0300

    Payment

--- src/main/java/com/cursoSpring/course/test/TestConfig.java
+++ src/main/java/com/cursoSpring/course/test/TestConfig.java
@@ -11,6 +11,7 @@ import org.springframework.context.annotation.Profile;
 import com.cursoSpring.course.entities.Category;
 import com.cursoSpring.course.entities.Order;
 import com.cursoSpring.course.entities.OrderItem;
+import com.cursoSpring.course.entities.Payment;
 import com.cursoSpring.course.entities.Product;
 import com.cursoSpring.course.entities.User;
 import com.cursoSpring.course.enuns.OrderStatus;
@@ -79,5 +80,9 @@ public class TestConfig implements CommandLineRunner {
 		OrderItem oi4 = new OrderItem(o3, p5, 2, p5.getPrice());
 
 		orderItemRepository.saveAll(Arrays.asList(oi1, oi2, oi3, oi4));
+		
+		Payment pay1 = new Payment(null,Instant.parse("2019-06-20T21:53:07Z"),o1 );
+		o1.setPayment(pay1);
+		orderRepository.save(o1);
 	}
 }
